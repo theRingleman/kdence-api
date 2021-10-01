f914acf43fe86d46ec97e9bbff519462975c5d12
Sam Ringleman <sam.ringleman@gmail.com>
2021-10-01 07:56:14 -0600

    Boot strap users entity and setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { UsersModule } from './users/users.module';
 import { HouseholdsModule } from './households/households.module';
 import { GoalsModule } from './goals/goals.module';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/user.entity';
 
 @Module({
-  imports: [UsersModule, HouseholdsModule, GoalsModule, TasksModule],
+  imports: [
+    UsersModule,
+    HouseholdsModule,
+    GoalsModule,
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'kdence',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
