0ccad9a411666a02eeabb16f99e2017030ddf3de
Sam Ringleman <sam.ringleman@gmail.com>
2021-10-06 12:02:14 -0600

    Add auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findOneByEmail(email);
+    if (await bcrypt.compare(password, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
