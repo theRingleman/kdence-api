2cd787ac71b818710db12d6010e0bb29db18a6ea
Sam Ringleman <sam.ringleman@gmail.com>
2021-10-23 01:07:59 -0600

    add docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,8 +22,12 @@ import { AuthModule } from './auth/auth.module';
     GoalsModule,
     TasksModule,
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DB,
       entities: [
         UserEntity,
         HouseholdEntity,
