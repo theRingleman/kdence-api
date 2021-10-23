cfdd660333f5a82f44b41808a9623c02a5e84d6f
Sam Ringleman <sam.ringleman@gmail.com>
2021-10-23 08:29:05 -0600

    Add ssl for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,11 +23,7 @@ import { AuthModule } from './auth/auth.module';
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DB,
+      url: process.env.DB_URL,
       entities: [
         UserEntity,
         HouseholdEntity,
